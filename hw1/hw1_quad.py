b486f1793578020227a1350b26ddc28b596f55a7
codlocker <isahoo1@asu.edu>
2023-08-27 08:16:30 -0700

    Add some update bro

--- hw1/hw1_quad.py
+++ hw1/hw1_quad.py
@@ -3,15 +3,15 @@ import numpy as np
 
 # This program gets the roots of a quadriatic equation.
 
-def get_determinant(a: int, b: int, c: int) -> int:
-    # Get determinant of a quadriatic equation.
+def get_discriminant(a: int, b: int, c: int) -> int:
+    # Get discriminant of a quadriatic equation.
     # Args:
     #     a (int): First coefficient of a quadriatic equation.
     #     b (int): Second coefficient of a quadriatic equation.
     #     c (int): Third coefficient of a quadriatic equation.
 
     # Returns:
-    #     int: The determinant of the quadriatic equation. 
+    #     int: The discriminant of the quadriatic equation. 
 
     return b**2 - 4 * a * c
 
@@ -19,33 +19,35 @@ if __name__ == "__main__":
     a = int(input("Input coefficient a : "))
     b = int(input("Input coefficient b : "))
     c = int(input("Input coefficient c : "))
+    TOLERANCE = 1e-5
 
     if a == 0:
         if b == 0:
-            print("x can take value in the real number space.")
+            print("x can take any value in the real number space.")
             exit(0)
         else:
             root = -c / b
             print("Single Root :", root)
             exit(0)
 
-    det = get_determinant(a, b, c)
+    det = get_discriminant(a, b, c)
+    delta = det - 0.0
 
-    if det < 0:
+    if det < 0 and abs(delta) > TOLERANCE:
         root = cmath.sqrt(det)
 
         rootA = (-b + root) / (2 * a)
         rootB = (-b - root) / (2 * a)
         print("Root 1:", rootA)
         print("Root 2:", rootB)
-    elif det > 0:
+    elif det > 0 and abs(delta) > TOLERANCE:
         root = np.sqrt(det)
         rootA = (-b + root) / (2 * a)
         rootB = (-b - root) / (2 * a)
 
         print("Root 1:", rootA)
         print("Root 2:", rootB)
-    else:
+    elif abs(delta) < TOLERANCE:
         root = -b / (2 * a)
 
         print("Double root:", root)
