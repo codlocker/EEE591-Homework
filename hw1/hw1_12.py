0fcd70a10cd208650648a0631208da45b17f341f
Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>
2023-08-29 21:37:05 -0700

    HW1:Add more comments

--- hw1/hw1_12.py
+++ hw1/hw1_12.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-# This program calculates the prime numbers from 1 upto the given number.
-# The number given as input here is 10000.
+##########################################################################
+# This program calculates the prime numbers from 1 upto the given number.#
+# The number given as input here is 10000.                               #
+##########################################################################
+
 def get_primes(n: int) -> list[int]:
     
     # Generate prime numbers for all numbers to at most n
@@ -15,12 +18,16 @@ def get_primes(n: int) -> list[int]:
     if n < 2:
         return []
     
+    # This is a boolean array that stores which numbers are prime / non-prime
+    # from 0 to n.
+  
     is_prime = [True] * (n + 1)
     is_prime[0] = False
     is_prime[1] = False
 
     for i in range(2, int(np.sqrt(n + 1))):
         if is_prime[i]:
+            # Loop through multiples and mark them as non-prime.
             for x in range(i * i, n + 1, i):
                 is_prime[x] = False 
 
