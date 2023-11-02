89568c697629a37daf56b51cfbd8ff258f62e5a0
Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>
2023-11-02 01:50:42 -0700

    Result 3

--- proj3/proj3.py
+++ proj3/proj3.py
@@ -10,6 +10,11 @@ warnings.filterwarnings('ignore')
 Q = ct.physical_constants['atomic unit of charge'][0]
 KB = ct.physical_constants['Boltzmann constant'][0]
 
+###########################################################################
+# Problem 1 : Determine the current, I, thorough the circuit in Figure 1  #
+# by doing a  nodal analysis for the circuit and using the current through#
+#  the diode defined by the diode equation.                               #
+###########################################################################
 
 # Constants for Problem 1
 # Saturation Current
@@ -39,6 +44,17 @@ def Idiode(
     return Is * (np.exp((Q * V) / (n * KB * T)) - 1)
 
 # Solve for diode voltage
+#    Args:
+#        prev_v (float): voltage start for guess
+#        src_v (float): source voltage
+#        r_val (int): Resitance value
+#        ideality (float): ideality value.
+#        temp (int): Temerature of diode
+#        isat_val (float): Saturation current
+
+#    Returns:
+#        _type_: _description_
+#
 def solve_diode_v(
     prev_v: float,
     src_v: float,
@@ -52,30 +68,25 @@ def solve_diode_v(
     # Solve diode voltage
     return (prev_v - src_v) / r_val + diode_current
 
-
-
+# Step 1: Solve for diode voltage
 diode_voltage = fsolve(solve_diode_v, V, args=(init_voltage, R_1, N, T_1, I_SAT))
-diode_voltage
 
-
-
-
+# Step 2: Solve for diode current
 diode_current = Idiode(
     V=diode_voltage,
     n=N,
     T=T_1,
     Is=I_SAT)
-diode_current
 
-
-
-
+# 3. Store the log value for plotting graph.
 log_diode_current = np.log10(diode_current)
 
 
+###########################################################################
+# Problem 2 : You are supplied a data set named DiodeIV.txt. The objective#
+# is to find the three missing diode parameters.                          #
+###########################################################################
 
-
-# Problem 2
 # Resistance
 R_2 = 10000
 
@@ -100,9 +111,6 @@ THRESHOLD = 1e-6
 # MAx iterations
 MAX_ITER = 1e6
 
-
-
-
 # Read the contents of diode.txt file
 data = pd.read_csv('DiodeIV.txt', header=None, sep=' ')
 source_voltages = []
@@ -116,9 +124,18 @@ measured_current = np.asarray(measured_current)
 
 
 
-
-print("Measured Current: ", measured_current)
-
+# Calculate the current in the diode
+#    Args:
+#        A (float): Area
+#        phi (float): phi value
+#        R (float): Resitance
+#        n (float): ideality value
+#        temp (float): Temperature of the diode
+#        v_s (np.array): source voltages
+#
+#    Returns:
+#        _type_: current in diode
+#
 def solve_current_diode(
     A: float, 
     phi: float,
@@ -126,7 +143,6 @@ def solve_current_diode(
     n: float, 
     temp: float,
     v_s: np.array):
-    
     # create zero array to store computed diode current/voltage
     diode_voltage_est = np.zeros_like(v_s)
     current_diode = np.zeros_like(v_s)
@@ -144,19 +160,43 @@ def solve_current_diode(
     current_diode = is_val * (np.exp(diode_voltage_est / vt) - 1) # calc diode current by its definition
     return current_diode
 
+# Optimize the resistance value
+# Args:
+#    R_guess (float): Ideality guess
+#    phi_guess (float): phi guess
+#    n_guess (float): n guess
+#    A (float): Area
+#    T (int): Temperature in kelvin
+#    v_src (np.array): Source voltages.
+#    current_meas (np.array): Current measured from the file
+
+# Returns:
+#     _type_: _description_
 def opt_r(R_guess: float, 
           phi_guess: float,
           n_guess: float,
           A: float,
           T: int,
           v_src: np.array,
-          current_meas: np.array):  
+          current_meas: np.array):
     # diode current is obtained using optimized parameters
     current_diode = solve_current_diode(A, phi_guess, R_guess, n_guess, T, v_src)
     # Absolute error
-    return (current_diode - measured_current)
+    return (current_diode - current_meas)
 
-    
+# Optimize the Phi value
+
+#    Args:
+#        phi_guess (float): Guess for phi
+#        R_guess (float): Guess for R
+#        n_guess (float): Guess for n
+#        A (float): Area of diode
+#        T (int): Temperature of diode.
+#        v_src (np.array): Source voltages.
+#        current_meas (np.array): Measured current.
+
+#    Returns:
+#        _type_: returns optimized res
 def opt_phi(phi_guess: float,
             R_guess: float,
             n_guess: float,
@@ -167,8 +207,20 @@ def opt_phi(phi_guess: float,
     # diode current is obtained using optimized parameters
     current_diode = solve_current_diode(A, phi_guess, R_guess, n_guess, T, v_src)
     # Normalized error is obtained by adding a contant in denominator for handling 0/0 case
-    return (current_diode - measured_current) / (current_diode + measured_current + 1e-15)
-    
+    return (current_diode - current_meas) / (current_diode + measured_current + 1e-15)
+
+# Optimize the ideality
+#    Args:
+#        n_guess (float): Guess the ideality.
+#        R_guess (float): Guess the resistance.
+#        phi_guess (float): Guess the phi value.
+#        A (float): Guess the Area.
+#        T (int): the temperature of diode.
+#        v_src (np.array): source voltages.
+#        current_meas (np.array): measured current.
+
+#    Returns:
+#        _type_: optimized ideality
 def opt_n(n_guess: float,
           R_guess: float,
           phi_guess: float,
@@ -179,7 +231,7 @@ def opt_n(n_guess: float,
     # diode current is obtained using optimized parameters
     current_diode = solve_current_diode(A, phi_guess, R_guess, n_guess, T, v_src)
     # Normalized error is obtained by adding a contant in denominator for handling 0/0 case
-    return (current_diode - measured_current) / (current_diode + measured_current + 1e-15)
+    return (current_diode - current_meas) / (current_diode + measured_current + 1e-15)
 
 
 err = 10
@@ -203,8 +255,7 @@ while err > THRESHOLD and iteration < MAX_ITER:
     err = np.linalg.norm((current_pred - measured_current) / (current_pred + measured_current + 1e-15), ord = 1)
     
     # Print iteration progress
-    if iteration % 1 == 0:
-        print(f"Iter#: {iteration} ; phi: {phi_val} ; n: {n_val} ; R: {r_val} ; Residual Error: {err}")
+    print(f"Iter#: {iteration} ; phi: {phi_val} ; n: {n_val} ; R: {r_val} ; Residual Error: {err}") 
 
     # Increment the iteration count
     iteration += 1
@@ -218,8 +269,6 @@ print(f"ESTIMATED Phi : {phi_val}")
 print(f"ESTIMATED Ideality factor (n) : {n_val}")
 
 
-
-
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
 # Problem 1 graph
 ax1.plot(init_voltage, log_diode_current, label="Source Voltage vs Diode current", marker="*")
@@ -229,7 +278,7 @@ ax1.set_xlabel("Diode voltage (volts)")
 ax1.set_title("Problem 1 plot")
 ax1.legend()
 
-# Set axes params for the plot
+# Problem 2 graph: Set axes params for the plot
 ax2.set_xlabel("Diode voltage in volts")
 ax2.set_ylabel("Diode current in log scale")
 ax2.set_title("Problem 2 plot")
