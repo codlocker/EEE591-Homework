cc9a57b485641727bf41eafbe8ac6b88c24cacec
Ipsit Sahoo <12949012+codlocker@users.noreply.github.com>
2023-11-13 15:51:57 -0700

    Update comments (#41)
    Co-authored-by: Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>

--- proj4/utils.py
+++ proj4/utils.py
@@ -1,5 +1,8 @@
 import os
 
+# Create a directory in a given path in Python
+# Args:
+#    directory_path (str): The directory path.
 def create_directory(directory_path: str):
     # Check if the directory already exists
     if not os.path.exists(directory_path):
@@ -9,6 +12,9 @@ def create_directory(directory_path: str):
     else:
         print(f"Directory '{directory_path}' already exists.")
 
+# Delete all files in a directory as a clean-up process.
+# Args:
+#   path (str): Delete all files in a particular path.
 def delete_all_files_in_dir(path: str):
     files = os.listdir(path=path)
 
@@ -18,6 +24,11 @@ def delete_all_files_in_dir(path: str):
         if os.path.exists(f"{path}/{file}"):
             os.unlink(f"{path}/{file}")
 
+# Validate all files if they exist.
+# Args:
+#    files (list): List of files with relative paths to look for in the code.
+# Returns:
+#    bool: if all paths are valid.
 def validate_paths(files: list) -> bool:
     curr_wd = os.getcwd()
 
